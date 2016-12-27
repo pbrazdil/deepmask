2c28db6f2771e523c5f959e7e9ec3dc8badd1e84
Petr Brazdil <p.brazdil@gmail.com>
2016-12-27 15:19:01 +0100

    Add support for custom categories in mscoco

---
+++ prepareData.py
@@ -0,0 +1,34 @@
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import os.path
+import sys
+
+if len(sys.argv) <= 1:
+  print("Data Type argument needs to be given.")
+  sys.exit(1)
+
+dataDir = './data'
+dataType = sys.argv[1]
+
+fdir = '%s/%s' % (dataDir, dataType)
+if not os.path.exists(fdir):
+  os.makedirs(fdir)
+
+annotationFile = '%s/annotations/instances_%s.json' % (dataDir, dataType)
+
+coco = COCO(annotationFile)
+catIds = coco.getCatIds(catNms=['stop sign']);
+imgIds = coco.getImgIds(catIds=catIds);
+i = 1
+for val in imgIds:
+  print("%d / %d" % (i, len(imgIds)))
+  id = str(val)
+  fname = '%s/COCO_%s_%s.jpg' % (fdir, dataType, "0" * (12-len(id)) + id)
+  if not os.path.exists(fname):
+    img = io.imread('http://mscoco.org/images/%s' % (id))
+    io.imsave(fname, img)
+  
+  i += 1
+
+print("Download done")
